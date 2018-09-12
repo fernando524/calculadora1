d794dc1df3dceb27231379c1579e2cbc46004ccc
fernando524 <42592733+fernando524@users.noreply.github.com>
2018-09-11 21:32:20 -0300

    Update test_x.py

--- test_x.py
+++ test_x.py
@@ -4,7 +4,7 @@ from app import soma, subtracao, multiplicacao
 
 
 def test_soma():
-	assert soma(2,2) == 4
+	assert soma(2,2) == 6
 def test_subtracao():
 	assert subtracao(5,2) == 3
 def test_multiplicacao():
